8e5a841bf578230bab5c6bccb0488bba735ee116
Ther-nullptr <1329438302@qq.com>
2022-11-20 15:04:57 +0800

    chore(test): add test file for classification

---
+++ test/beit_image_classification.py
@@ -0,0 +1,22 @@
+from transformers import BeitFeatureExtractor, BeitForImageClassification
+from PIL import Image
+from tqdm import tqdm
+import os
+
+img_dir = '/root/kyzhang/yjwang/InclusiveFL2/data/Dataset/Test/Clean/img'
+img_names = os.listdir(img_dir)
+img_names.sort()
+
+for img_name in tqdm(img_names):
+    print(img_name)
+    image = Image.open(os.path.join(img_dir, img_name))
+    feature_extractor = BeitFeatureExtractor.from_pretrained('/root/kyzhang/download/beit-base-patch16-224-pt22k-ft22k')
+    model = BeitForImageClassification.from_pretrained('/root/kyzhang/download/beit-base-patch16-224-pt22k-ft22k')
+    inputs = feature_extractor(images=image, return_tensors="pt")
+    outputs = model(**inputs)
+    logits = outputs.logits
+    # model predicts one of the 21,841 ImageNet-22k classes
+    prob, predicted_class_idx = logits.topk(k=5, dim=-1)
+    for i in range(len(predicted_class_idx[0])):
+        print("Predicted class:", model.config.id2label[int(predicted_class_idx[0][i])])
+        print("Probility:", float(prob[0][i]))
